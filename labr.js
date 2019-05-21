1d4fdded91f7e5206c284e393633fa9bff3b8a53
Taylor Wong <taylorwong159@gmail.com>
2019-05-20 23:02:39 -0600

    made into node module

--- labr.js
+++ labr.js
@@ -1,131 +1,134 @@
+module.exports = {
 
-// Dot multiply
-function vectorDotProduct(a, b) {
-  let result = 0;
-  let shorter;
-  let longer;
-  let diff;
-  let valid = true
+  // Dot multiply
+  vectorDotProduct: function (a, b) {
+    let result = 0;
+    let shorter;
+    let longer;
+    let diff;
+    let valid = true
 
-  // Validate vectors
-  function check(vector) {
+    // Validate vectors
+    function check(vector) {
+      for (let i = 0; i < vector.length; i++) {
+        if (typeof vector[i] != 'number') {
+          valid = false;
+        }
+      }
+    }
+    check(a);
+    check(b);
+
+    if (valid) {
+
+    // Handle different vector length
+      if (a.length > b.length) {
+        longer = a;
+        shorter = b;
+      } else {
+        longer = b;
+        shorter = a;
+      }
+
+      diff = longer.length - shorter.length
+      for (let i = 0; i < diff; i++) {
+        shorter.push(0);
+      }
+      for (let i = 0; i < longer.length; i++) {
+        result += (longer[i] * shorter[i]);
+      }
+      return result;
+    } else {
+      return 'invalid vectors';
+    }
+
+  },
+
+  // Cross multiply
+  vectorCrossProduct: function (a, b) {
+    if (a.length === b.length && a.length === 3) {
+      let valid = true;
+      let resVector = [];
+      for (i = 0; i < 3; i++) {
+        if (typeof a[i] != 'number' || typeof b[i] != 'number') {
+          let valid = false;
+        }
+      }
+      if (valid) {
+        resVector.push((a[1] * b[2]) - (a[2] * b[1]));
+        resVector.push((a[2] * b[0]) - (a[0] * b[2]));
+        resVector.push((a[0] * b[1]) - (a[1] * b[0]));
+        return resVector;
+      } else {
+        return 'invalid verctors';
+      }
+    }
+    return 'invalid vectors';
+  },
+
+  // Transpose
+  transpose: function (matrix) {
+    let temp = [[]];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (temp.length - 1 < j) {
+          temp.push([]);
+        }
+        temp[j].push(matrix[i][j]);
+      }
+    }
+    return temp;
+  },
+
+  // Transformation
+  transform: function (vector, matrix) {
+    let valid = true;
+    let resVector = [];
+    let transposedMatrix = transpose(matrix);
+
+    // Validate
+    if (matrix.length != vector.length) {
+      valid = false;
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i].length != vector.length) {
+        valid = false;
+      }
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (typeof matrix[i][j] != 'number') {
+          valid = false;
+        }
+      }
+    }
     for (let i = 0; i < vector.length; i++) {
       if (typeof vector[i] != 'number') {
         valid = false;
       }
     }
-  }
-  check(a);
-  check(b);
 
-  if (valid) {
-
-  // Handle different vector length
-    if (a.length > b.length) {
-      longer = a;
-      shorter = b;
-    } else {
-      longer = b;
-      shorter = a;
-    }
-
-    diff = longer.length - shorter.length
-    for (let i = 0; i < diff; i++) {
-      shorter.push(0);
-    }
-    for (let i = 0; i < longer.length; i++) {
-      result += (longer[i] * shorter[i]);
-    }
-    return result;
-  } else {
-    return 'invalid vectors';
-  }
-
-}
-
-// Cross multiply
-function vectorCrossProduct(a, b) {
-  if (a.length === b.length && a.length === 3) {
-    let valid = true;
-    let resVector = [];
-    for (i = 0; i < 3; i++) {
-      if (typeof a[i] != 'number' || typeof b[i] != 'number') {
-        let valid = false;
-      }
-    }
+    // Calculate
     if (valid) {
-      resVector.push((a[1] * b[2]) - (a[2] * b[1]));
-      resVector.push((a[2] * b[0]) - (a[0] * b[2]));
-      resVector.push((a[0] * b[1]) - (a[1] * b[0]));
+      for (let i = 0; i < transposedMatrix.length; i++) {
+        resVector.push(vectorDotProduct(transposedMatrix[i], vector));
+      }
       return resVector;
     } else {
-      return 'invalid verctors';
+      return 'invalid vector and/or matrix';
     }
-  }
-  return 'invalid vectors';
-}
+  },
 
-// Transpose
-function transpose(matrix) {
-  let temp = [[]];
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (temp.length - 1 < j) {
-        temp.push([]);
-      }
-      temp[j].push(matrix[i][j]);
-    }
-  }
-  return temp;
-}
-
-// Transformation
-function transform(vector, matrix) {
-  let valid = true;
-  let resVector = [];
-  let transposedMatrix = transpose(matrix);
-
-  // Validate
-  if (matrix.length != vector.length) {
-    valid = false;
-  }
-
-  for (let i = 0; i < matrix.length; i++) {
-    if (matrix[i].length != vector.length) {
-      valid = false;
-    }
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (typeof matrix[i][j] != 'number') {
-        valid = false;
+  // Find length
+  vectorLength: function (vector) {
+    let sum = 0;
+    for (let i = 0; i < vector.length; i++) {
+      if (typeof vector[i] == 'number') {
+        sum += vector[i] ** 2;
+      } else {
+        return 'invalid vector';
       }
     }
-  }
-  for (let i = 0; i < vector.length; i++) {
-    if (typeof vector[i] != 'number') {
-      valid = false;
-    }
+    return sum ** 0.5;
   }
 
-  // Calculate
-  if (valid) {
-    for (let i = 0; i < transposedMatrix.length; i++) {
-      resVector.push(vectorDotProduct(transposedMatrix[i], vector));
-    }
-    return resVector;
-  } else {
-    return 'invalid vector and/or matrix';
-  }
-}
-
-// Find length
-function vectorLength(vector) {
-  let sum = 0;
-  for (let i = 0; i < vector.length; i++) {
-    if (typeof vector[i] == 'number') {
-      sum += vector[i] ** 2;
-    } else {
-      return 'invalid vector';
-    }
-  }
-  return sum ** 0.5;
 }
